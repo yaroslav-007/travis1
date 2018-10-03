7079686a542da3e2052c016e7ecd43412114eabf
Yaroslav <yorimark@abv.bg>
2018-10-03 16:11:15 +0300

    adding changes to file

--- Bye.sh
+++ Bye.sh
@@ -2,8 +2,8 @@
 
 out=$(./hello.sh)
 
-# we should check out == hello
-if [ "${out}" == "hello" ];then
+# we should check out == bye
+if [ "${out}" == "bye" ];then
   echo "GOOD: test pass"
 else
   echo "BAD: test fail"
